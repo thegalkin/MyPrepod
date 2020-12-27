351c4f72e376c5ecb04a221558c9aeb8e1158b31
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2020-12-27 16:40:41 +0400

    Initial Commit

---
+++ MyPrepod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPrepod
+//
+//  Created by Никита Галкин on 27.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPrepod/MyPrepodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPrepodApp.swift
+//  MyPrepod
+//
+//  Created by Никита Галкин on 27.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct MyPrepodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
